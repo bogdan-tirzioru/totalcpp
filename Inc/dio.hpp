8416577ccf73e754f035efc46331933db868ab3d
BoTi <bogdan_tirzioru@yahoo.com>
2022-12-23 21:02:42 +0200

    refine class dio with pins and ports

--- Inc/dio.hpp
+++ Inc/dio.hpp
@@ -9,9 +9,27 @@
 #define DIO_HPP_
 
 #include "driver.hpp"
+class Pin{
+public:
+	Pin(){;};
+};
+class Port{
+	const unsigned char DataPortWidth = 32;
+	Pin data[DataPortWidth];
+public:
+	Port(){;};
+};
 
 class Dio:public Driver{
 	bool value=false;
+	Port PA;
+	Port PB;
+	Port PC;
+	Port PD;
+	Port PE;
+	Port PF;
+	Port PG;
+
 public:
 	Dio(void){;};
 	void Toggle(void){value= !value;};
