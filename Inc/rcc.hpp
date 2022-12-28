9e5bd92dc8ab3c6fa15592c526b6441763082af0
BoTi <bogdan_tirzioru@yahoo.com>
2022-12-28 12:16:28 +0200

    select HSE as clock source

--- Inc/rcc.hpp
+++ Inc/rcc.hpp
@@ -13,14 +13,27 @@ enum class TypeRccSource { HSE,HSI};
 class Rcc : public Device
 {
 	RCC_TypeDef *reg;
+	TypeRccSource selsourceclk;
 public:
 	Rcc(TypeRccSource sourceclk,unsigned long value)
 	{
+		selsourceclk = sourceclk;
 		reg = RCC;
-
 	}
 	virtual void Setup(void)
 	{
+		switch (selsourceclk)
+		{
+			case TypeRccSource::HSE:
+				reg->CR |= RCC_CR_HSEON;
+				break;
+			case TypeRccSource::HSI:
+				reg->CR |= RCC_CR_HSION;
+			default:
+				break;
+
+		};
+
 
 	}
 	virtual ~Rcc(void)
