d124e14877d475eac0863e8485568248f3aaca7e
BoTi <bogdan_tirzioru@yahoo.com>
2022-12-29 22:19:30 +0200

    activate the pll1 and set 480Mhz out

--- Inc/rcc.hpp
+++ Inc/rcc.hpp
@@ -22,6 +22,7 @@ public:
 	}
 	virtual void Setup(void)
 	{
+		/*select the clock source*/
 		switch (selsourceclk)
 		{
 			case TypeRccSource::HSE:
@@ -33,12 +34,38 @@ public:
 				break;
 
 		};
+		/*setup the PLL1*/
+		/*set the source HSE*/
+		reg->PLLCKSELR |= RCC_PLLCKSELR_PLLSRC_HSE;
+		/*select the divider for PLL1*/
+		reg->PLLCKSELR &= ~RCC_PLLCKSELR_DIVM1;
+		reg->PLLCKSELR |= RCC_PLLCKSELR_DIVM1_0;
+		/*select the range to 8-16MHz*/
+		reg->PLLCFGR |= RCC_PLLCFGR_PLL1RGE_3;
+		/*wide range selected to 192 to 836 MHz*/
+		reg->PLLCFGR &= ~RCC_PLLCFGR_PLL1VCOSEL;
+		/*disable the fractional divider*/
+		reg->PLLCFGR &= ~RCC_PLLCFGR_PLL1FRACEN;
+		/*enable the P Q and R factor*/
+		reg->PLLCFGR |= RCC_PLLCFGR_DIVP1EN;
+		reg->PLLCFGR |= RCC_PLLCFGR_DIVQ1EN;
+		reg->PLLCFGR |= RCC_PLLCFGR_DIVR1EN;
+
+		/*set multiplier to 60*/
+		reg->PLL1DIVR &= ~RCC_PLL1DIVR_N1;
+		reg->PLL1DIVR |= (60-1) <<RCC_PLL1DIVR_N1_Pos;
+
+		reg->PLL1DIVR &= ~RCC_PLL1DIVR_P1;
+		reg->PLL1DIVR &= ~RCC_PLL1DIVR_Q1;
+		reg->PLL1DIVR &= ~RCC_PLL1DIVR_R1;
+
+		reg->CR |= RCC_CR_PLL1ON;
+		while ((reg->CR & RCC_CR_PLL1RDY) != RCC_CR_PLL1RDY);
 
 
 	}
 	virtual ~Rcc(void)
 	{
-
 	}
 };
 
